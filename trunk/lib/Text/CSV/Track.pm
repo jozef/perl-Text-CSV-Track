2b96bb1cee1fdee6cd6633bfec0b1feefd6b264b
jk <jk@b0c1b02c-7ba7-4df7-b273-855bf36df2ab>
2006-12-27 14:56:19 +0000

    full time lock option
    git-svn-id: https://cle.sk/repos/pub/Text-CSV-TrackMAX@10 b0c1b02c-7ba7-4df7-b273-855bf36df2ab

--- trunk/lib/Text/CSV/Track.pm
+++ trunk/lib/Text/CSV/Track.pm
@@ -45,6 +45,7 @@ and can return the numeric value for the identificator or update it
 	new({
 		file_name           => 'filename.csv',
 		ignore_missing_file => 1,
+		full_time_lock      => 1,
 		
 	})
 	
@@ -54,6 +55,9 @@ and can return the numeric value for the identificator or update it
 
 	if 'ignore_missing_file' is set then the lib will just warn that it can not
 	read the file. store() will use this name to store the results.
+	
+	if 'full_time_lock' is set the exclusive lock will be held until the object is
+	not destroyed.
 
 =item value_of()
 
@@ -88,7 +92,7 @@ use strict;
 use warnings;
 
 use base qw(Class::Accessor::Fast);
-Text::CSV::TrackMAX->mk_accessors(qw(file_name rh_value_of lazy_init ignore_missing_file));
+Text::CSV::Track->mk_accessors(qw(file_name file_fh rh_value_of lazy_init ignore_missing_file full_time_lock));
 
 use FindBin;
 
@@ -96,48 +100,42 @@ use Text::CSV;
 use Carp::Clan;
 use English qw(-no_match_vars);
 use Fcntl ':flock'; # import LOCK_* constants
+use Fcntl ':seek';  # import SEEK_* constants
 
 #readonly defaults
 
-my $ACCESS_CSV_FORMAT = Text::CSV->new({
+my $CSV_FORMAT = Text::CSV->new({
 		sep_char    => q{,},
 		escape_char => q{\\},
 		quote_char  => q{"},
 	});
 
-#get or set access_time of active card user
+#get or set value
 sub value_of {
-	#arg
 	my $self          = shift;
 	my $indetificator = shift;
+	my $is_set = (@_ > 0 ? 1 : 0);	#get or set request depending on number of arguments
+	my $value = shift;
 
-	#separate get and set request depending on number of arguments
-	my $is_set = 0;
-	$is_set    = 1 if (@_ > 0);
-
-	#arg
-	my $max_value = shift;
-
+	#check if we have identificator
+	return if not $indetificator;
+	
 	#lazy initialization
 	$self->_init();
 			
-	
-	#check if we have ac_login
-	return if not $indetificator;
-	
-	#access_time_of hash
+	#value_of hash
 	my $rh_value_of = $self->{rh_value_of};
 
 	#switch between set and get variant
 	#set
 	if ($is_set) {
 		#if access_time is 'undef' then remove it from the hash
-		if (not defined $max_value) {
+		if (not defined $value) {
 			delete $rh_value_of->{$indetificator};
 		}
 		#update value in hash
 		else {
-			$rh_value_of->{$indetificator} = $max_value;
+			$rh_value_of->{$indetificator} = $value;
 		}
 	}
 	#get
@@ -154,24 +152,31 @@ sub store {
 	$self->_init();
 
 	#get local variables from self hash
-	my $rh_value_of = $self->{rh_value_of};
-	my $file_name   = $self->{file_name};
+	my $rh_value_of    = $self->{rh_value_of};
+	my $file_name      = $self->{file_name};
+	my $full_time_lock = $self->{full_time_lock};
+	my $file_fh        = $self->{file_fh};
 
-	open(my $file_fh, "+>>", $file_name) or croak "can't write to file '$file_name' - $OS_ERROR";
+	if (not $full_time_lock) {
+		open(my $file_fh, "+>>", $file_name) or croak "can't write to file '$file_name' - $OS_ERROR";
 	
-	#lock and truncate the access store file
-	flock($file_fh, LOCK_EX) or croak "can't lock file '$file_name' - $OS_ERROR\n";
+		#lock and truncate the access store file
+		flock($file_fh, LOCK_EX) or croak "can't lock file '$file_name' - $OS_ERROR\n";
+	}
+	
+	#truncate the file so that we can store new results
 	truncate($file_fh, 0) or croak "can't truncate file '$file_name' - $OS_ERROR\n";
 	
+	#loop through identificators and write to file
 	foreach my $indetificator (sort $self->ident_list()) {
 		#combine values for csv file
-		if (not $ACCESS_CSV_FORMAT->combine($indetificator, $rh_value_of->{$indetificator})) {
-			warn "invalid value to store to an csv file - ", $ACCESS_CSV_FORMAT->error_input(),"\n";
+		if (not $CSV_FORMAT->combine($indetificator, $rh_value_of->{$indetificator})) {
+			warn "invalid value to store to an csv file - ", $CSV_FORMAT->error_input(),"\n";
 			next;
 		}
 		
 		#print the line to csv file
-		print {$file_fh} $ACCESS_CSV_FORMAT->string(), "\n";
+		print {$file_fh} $CSV_FORMAT->string(), "\n";
 	}
 	
 	close($file_fh);
@@ -191,15 +196,28 @@ sub _init {
 	my $rh_value_of         = $self->{rh_value_of};
 	my $file_name           = $self->{file_name};
 	my $ignore_missing_file = $self->{ignore_missing_file};
+	my $full_time_lock      = $self->{full_time_lock};
 	
 	#done with initialization if file_name empty
 	return if not $file_name;
+
+	#default open mode is reading
+	my $open_mode = '<';
+	
+	#if full_time_lock is set do open for writting
+	if ($full_time_lock) {
+		if ($ignore_missing_file) {
+			$open_mode = '+>>';
+		}
+		else {
+			$open_mode = '<+';
+		}
+	}
 	
 	#open file with old stored access times and handle error
 	my $file_fh;
-	if (not open($file_fh, "<", $file_name)) {
+	if (not open($file_fh, $open_mode, $file_name)) {
 		if (defined $ignore_missing_file) {
-			warn "skipping reading of access file '$file_name' - $OS_ERROR\n";
 			return;
 		}
 		else {
@@ -207,26 +225,41 @@ sub _init {
 		}
 	}
 	
-	#do shared lock
-	flock($file_fh, LOCK_SH) or croak "can't lock file '$file_name' - $OS_ERROR\n";
+	#do exclusive lock if full time lock
+	if ($full_time_lock) {
+		flock($file_fh, LOCK_EX) or croak "can't lock file '$file_name' - $OS_ERROR\n";
+		seek($file_fh, 0, SEEK_SET);
+	}
+	#otherwise shared lock is enought
+	else {
+		flock($file_fh, LOCK_SH) or croak "can't lock file '$file_name' - $OS_ERROR\n";
+	}
 
 	#parse lines and store values in the hash
 	LINE:
 	while (my $line = <$file_fh>) {
 		#verify line. if incorrect skip with warning
-		if (!$ACCESS_CSV_FORMAT->parse($line)) {
+		if (!$CSV_FORMAT->parse($line)) {
 			warn "badly formated '$file_name' csv line ", $file_fh->input_line_number(), " - '$line'. skipping\n";
 			next LINE;
 		}
 		
 		#extract fields
-		my ($indetificator, $max_value) = $ACCESS_CSV_FORMAT->fields();
+		my ($indetificator, $value) = $CSV_FORMAT->fields();
 
 		#update access time
-		$self->value_of($indetificator, $max_value);
+		$self->value_of($indetificator, $value);
 	}
 	
-	close($file_fh);
+	#if full time lock the store file handle
+	if ($full_time_lock) {
+		$self->{file_fh} = $file_fh;
+	}
+	#otherwise release shared lock and close file
+	else {
+		flock($file_fh, LOCK_UN);
+		close($file_fh);
+	}
 }
 
 sub ident_list {
@@ -240,3 +273,23 @@ sub ident_list {
 
 	return keys %{$rh_value_of};
 }
+
+sub finish {
+	my $self = shift;
+
+	#get local variables from self hash
+	my $file_fh = $self->{file_fh};
+
+	if ($file_fh) {
+		close($file_fh);
+	}	
+
+	$self->{file_fh} = undef;
+}
+
+sub DESTROY {
+	my $self = shift;
+
+	$self->finish();	
+}
+
